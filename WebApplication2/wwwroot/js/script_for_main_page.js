55cd0b35571d3f22e0be076464dfca0abf084a27
1238619 <1238619@mtp.by>
2026-01-13 17:49:56 +0300

    Добавьте файлы проекта.

---
+++ WebApplication2/wwwroot/js/script_for_main_page.js
@@ -0,0 +1,23 @@
+﻿function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const main = document.getElementById('mainContent');
+    sidebar.classList.toggle('open');
+    main.classList.toggle('shift');
+}
+
+
+function chooseAvatar() {
+    document.getElementById('avatarInput').click();
+}
+
+document.getElementById('avatarInput').addEventListener('change', function (e) {
+    const file = e.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (ev) {
+            document.getElementById('headerAvatar').src = ev.target.result;
+            document.getElementById('sidebarAvatar').src = ev.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
